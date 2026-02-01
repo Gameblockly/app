3d88de8a27d612412bec61bdaa7a70efcaa64785
velcro3 <leoncatpl27272@proton.me>
2026-02-01 12:19:57 +0000

    Starting to get back up with Builder.

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
+    win.loadFile(path.join(__dirname, 'src', 'index.html'));
+}
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+    if (process.platform !== "darwin") app.quit();
+});
